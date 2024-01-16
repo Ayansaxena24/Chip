372e5233a49508b57f41638d80b157d30a2c48f6
IAmVengeance <ayan.saxena24@gmail.com>
2024-01-16 22:15:01 +0530

    completed the functonalities

--- .history/src/App_20240116024335.tsx
+++ .history/src/App_20240116024335.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import ChipInput from './ChipInput'
+import ChipInput from './chipInput'
 
 const App = () => {
   return (
